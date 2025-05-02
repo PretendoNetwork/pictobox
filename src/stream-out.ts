e69997b8019f6937d64f5d69786f52581f0fc606
mirusu400 <mirusu400@naver.com>
2025-05-02 21:24:33 +0900

    chore: add TSDoc comment in stream-in.ts, stream-out.ts

--- src/stream-out.ts
+++ src/stream-out.ts
@@ -1,30 +1,54 @@
-// TODO - TSDoc comments
-
+/**
+ * A class for writing binary data to a Buffer
+ */
 export default class StreamOut {
 	private buffer: Buffer;
 	public pos: number;
 
+	/**
+	 * Creates a new StreamOut instance with an empty buffer
+	 */
 	constructor() {
 		this.buffer = Buffer.alloc(0);
 		this.pos = 0;
 	}
 
+	/**
+	 * Gets the current buffer
+	 * @returns The current buffer
+	 */
 	public bytes(): Buffer {
 		return this.buffer;
 	}
 
+	/**
+	 * Gets the total size of the buffer
+	 * @returns The total size of the buffer in bytes
+	 */
 	public size(): number {
 		return this.buffer.length;
 	}
 
+	/**
+	 * Skips the specified number of bytes by writing zeros
+	 * @param length - Number of bytes to skip
+	 */
 	public skip(length: number): void {
 		this.writeBytes(Buffer.alloc(length));
 	}
 
+	/**
+	 * Sets the current write position
+	 * @param pos - The new write position
+	 */
 	public seek(pos: number): void {
 		this.pos = pos;
 	}
 
+	/**
+	 * Writes bytes to the buffer
+	 * @param bytes - The bytes to write
+	 */
 	public writeBytes(bytes: Buffer): void {
 		const before = this.buffer.subarray(0, this.pos);
 		const after = this.buffer.subarray(this.pos);
@@ -38,6 +62,10 @@ export default class StreamOut {
 		this.pos += bytes.length;
 	}
 
+	/**
+	 * Writes an unsigned 8-bit integer
+	 * @param uint8 - The value to write
+	 */
 	public writeUint8(uint8: number): void {
 		const bytes = Buffer.alloc(1);
 
@@ -46,6 +74,10 @@ export default class StreamOut {
 		this.writeBytes(bytes);
 	}
 
+	/**
+	 * Writes an unsigned 16-bit integer in little-endian format
+	 * @param uint16 - The value to write
+	 */
 	public writeUint16LE(uint16: number): void {
 		const bytes = Buffer.alloc(2);
 
@@ -54,6 +86,10 @@ export default class StreamOut {
 		this.writeBytes(bytes);
 	}
 
+	/**
+	 * Writes an unsigned 32-bit integer in little-endian format
+	 * @param uint32 - The value to write
+	 */
 	public writeUint32LE(uint32: number): void {
 		const bytes = Buffer.alloc(4);
 
@@ -62,6 +98,10 @@ export default class StreamOut {
 		this.writeBytes(bytes);
 	}
 
+	/**
+	 * Writes a signed 32-bit integer in little-endian format
+	 * @param int32 - The value to write
+	 */
 	public writeInt32LE(int32: number): void {
 		const bytes = Buffer.alloc(4);
 
@@ -70,6 +110,10 @@ export default class StreamOut {
 		this.writeBytes(bytes);
 	}
 
+	/**
+	 * Writes an unsigned 16-bit integer in big-endian format
+	 * @param uint16 - The value to write
+	 */
 	public writeUint16BE(uint16: number): void {
 		const bytes = Buffer.alloc(2);
 
@@ -78,6 +122,10 @@ export default class StreamOut {
 		this.writeBytes(bytes);
 	}
 
+	/**
+	 * Writes an unsigned 32-bit integer in big-endian format
+	 * @param uint32 - The value to write
+	 */
 	public writeUint32BE(uint32: number): void {
 		const bytes = Buffer.alloc(4);
 
